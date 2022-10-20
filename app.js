74786ea40f061d5552513c71c9cd8a121e8a66bf
Lorenzo <lorenzozemp@icloud.com>
2022-10-20 21:25:00 +1300

    Added styling

---
+++ app.js
@@ -0,0 +1,10 @@
+const prevButton = document.getElementById("btn-prev");
+const nextButton = document.getElementById("btn-next");
+
+prevButton.addEventListener("click", function () {
+  console.log("previous review");
+});
+
+nextButton.addEventListener("click", function () {
+  console.log("next review");
+});
